9c6aae74944985558ccb6f2a53c39110f926cda9
Hữu Long <longanhminh9@gmail.com>
2023-04-29 23:38:07 +0700

    first commit

---
+++ scrip.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".multiple-text", {
+    strings:["Mobile Developer","............","Mobile Developer"],
+    typeSpeed: 100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true//(lặp lại) được thiết lập giá trị là true,
+    // cho phép chuỗi các ký tự sẽ được lặp lại liên tục.
+})
